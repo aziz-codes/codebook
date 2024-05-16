fc2b7c15ebff0c7935e08738c42a24ec6fe86ebc
aziz-codes <azizisoffline@gmail.com>
2024-05-16 23:56:53 +0500

    user component

---
+++ client/components/user.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+const user = () => {
+  return (
+    <div className="w-full flex border shadow-md rounded-md px-1 py-3">
+      <div className="flex gap-2 items-center">
+        <Avatar className="h-8 w-8">
+          <AvatarFallback>AZ</AvatarFallback>
+          <AvatarImage src="https://github.com/shadcn.png" />
+        </Avatar>
+        <div className="flex flex-col -space-y-1">
+          <h4 className="text-sm font-semibold">Aziz Khan</h4>
+          <p className="text-xs text-gray-400">@azizcodes</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default user;
