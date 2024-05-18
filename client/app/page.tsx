693cce42e4476a3cbebd1191275fa554041ea746
aziz-codes <azizisoffline@gmail.com>
2024-05-19 01:34:08 +0500

    active route detection

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import Post from "@/components/Post";
 const HomePage = () => {
-  return <Post />;
+  return (
+    <div className="w-full max-w-lg">
+      <Post />;
+    </div>
+  );
 };
 
 export default HomePage;
--- client/components/navlinks.tsx
+++ client/components/navlinks.tsx
@@ -1,9 +1,12 @@
+"use client";
 import React from "react";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { HomeIcon, User, TrendingUp, CodeXml } from "lucide-react";
+import { HomeIcon, Users, TrendingUp, CodeXml } from "lucide-react";
 import { Poppins } from "next/font/google";
 const poppins = Poppins({ subsets: ["latin"], weight: "500" });
 const navlinks = () => {
+  const pathName = usePathname();
   const links = [
     {
       label: "Home",
@@ -13,7 +16,7 @@ const navlinks = () => {
     {
       label: "Snippets",
       icon: CodeXml,
-      path: "/",
+      path: "/snippets",
     },
     {
       label: "Discussion",
@@ -21,8 +24,8 @@ const navlinks = () => {
       path: "/discussions",
     },
     {
-      label: "Devs",
-      icon: User,
+      label: "Peoples",
+      icon: Users,
       path: "/devs",
     },
   ];
@@ -33,7 +36,13 @@ const navlinks = () => {
         return (
           <Link
             href={link.path}
-            className={`${poppins.className} flex flex-row px-3 py-2 rounded-md items-center gap-2 transition-colors duration-200 bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700`}
+            className={`${
+              poppins.className
+            } flex flex-row px-3 py-2 rounded-md items-center gap-2 transition-colors duration-200 ${
+              link.path === pathName
+                ? "bg-gray-100 dark:bg-gray-700"
+                : "bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700"
+            }`}
             key={index}
           >
             <LinkIcon className="w-5 h-5 text-gray-700 dark:text-gray-300" />
