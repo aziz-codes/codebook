29fc051e26311020be238a60fa409b3a515d9877
aziz-codes <azizisoffline@gmail.com>
2024-05-19 17:00:27 +0500

    discussion header

--- client/app/discussions/page.tsx
+++ client/app/discussions/page.tsx
@@ -1,8 +1,10 @@
 import DiscussionCard from "@/components/discussion-card";
+import DiscussionHeader from "@/components/discussion-header";
 
 const Discussions = () => {
   return (
-    <div className="flex justify-center xl:justify-start">
+    <div className="flex  flex-col xl:items-start items-center gap-3">
+      <DiscussionHeader />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
         <DiscussionCard />
         <DiscussionCard />
