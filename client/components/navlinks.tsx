b04c6a1ae7e97e27054fb24e01f90bee7052255b
aziz-codes <azizisoffline@gmail.com>
2024-05-19 20:22:57 +0500

    new navlinks styling

--- client/components/navlinks.tsx
+++ client/components/navlinks.tsx
@@ -4,6 +4,8 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { HomeIcon, Users, TrendingUp, CodeXml, Gem } from "lucide-react";
 import { Poppins } from "next/font/google";
+import { buttonVariants } from "./ui/button";
+import { cn } from "@/lib/utils";
 const poppins = Poppins({ subsets: ["latin"], weight: "500" });
 const navlinks = () => {
   const pathName = usePathname();
@@ -35,23 +37,27 @@ const navlinks = () => {
     },
   ];
   return (
-    <div className="flex flex-col gap-3">
-      {links.map((link, index) => {
+    <div className="mt-5">
+      {links.map((link) => {
         const LinkIcon = link.icon;
+        const isActive = pathName === link.path;
+
         return (
           <Link
+            key={link.label}
             href={link.path}
-            className={`${
-              poppins.className
-            } flex flex-row px-3 py-2 rounded-md items-center gap-2 transition-colors duration-200 ${
-              link.path === pathName
-                ? "bg-gray-100 dark:bg-gray-700"
-                : "bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700"
-            }`}
-            key={index}
+            className={buttonVariants({
+              variant: isActive ? "secondary" : "ghost",
+              className: cn("navLink", { "hidden md:flex": null }),
+              size: "sm",
+            })}
           >
-            <LinkIcon className="w-5 h-5 text-gray-700 dark:text-gray-300" />
-            <p className="text-sm text-gray-700 dark:text-gray-300">
+            <LinkIcon className="w-5" />
+            <p
+              className={`${cn("hidden lg:block", {
+                "font-semibold": isActive,
+              })}`}
+            >
               {link.label}
             </p>
           </Link>
