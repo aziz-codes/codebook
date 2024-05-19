31d0b79ac853b595d78b47c8935bc9ff90dbd932
aziz-codes <azizisoffline@gmail.com>
2024-05-19 20:23:09 +0500

    discussion header

--- client/components/discussion-header.tsx
+++ client/components/discussion-header.tsx
@@ -4,6 +4,7 @@ import { Label } from "./ui/label";
 import DropDown from "./drop-down";
 import { Button } from "./ui/button";
 import { Plus, Search } from "lucide-react";
+import { Input } from "./ui/input";
 const DiscussionHeader = () => {
   const options = ["Javascript", "Python", "PHP", "CSS"];
   const handleSelect = (language: string) => {
@@ -26,11 +27,10 @@ const DiscussionHeader = () => {
         </div>
       </div>
       <div className="w-full flex flex-col md:flex-row gap-2 justify-between">
-        <div className="flex gap-1 flex-1 py-1 px-2 rounded-md border items-center justify-between ">
-          <input
-            type="text"
+        <div className="flex gap-1 flex-1 dark:bg-background py-1 px-2 rounded-md border items-center justify-between ">
+          <Input
             placeholder="Search"
-            className="outline-none border-0 px-1 w-full"
+            className="!outline-none border-0 px-1 w-full  !ring-0 h-full  focus:ring-0 focus:ring-offset-0 focus:bg-transparent"
           />
           <Search className="text-gray-400 h-5 w-5" />
         </div>
