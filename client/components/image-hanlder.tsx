aa9dd6e6ca86b579182ea604a4ed94c5b1d959f8
aziz-codes <azizisoffline@gmail.com>
2024-05-21 23:57:18 +0500

    image handler

---
+++ client/components/image-hanlder.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ImageHanlder = () => {
+  return (
+    <div className="flex mt-2">
+      <div className="flex-1 flex items-center justify-center">nekfn</div>
+      <div className="flex-1 flex items-center justify-center">nekfn</div>
+    </div>
+  );
+};
+
+export default ImageHanlder;
