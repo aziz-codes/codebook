1ab80cbd63d7ee28dbdd67df6460e29522543104
aziz-codes <azizisoffline@gmail.com>
2024-05-22 23:47:44 +0500

    config for random images

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "somehost",
       },
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+      },
     ],
   },
 };
