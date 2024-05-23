bea3f04eb7754ea3128d6c9d0ff18c963f655512
aziz-codes <azizisoffline@gmail.com>
2024-05-23 23:44:13 +0500

    middleware @v.0.1

---
+++ client/middleware.ts
@@ -0,0 +1,22 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse, NextRequest } from "next/server";
+
+export default async function middlware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const token = await getToken({
+    req: req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+  const publicPath = path === "/login";
+
+  if (publicPath && token) {
+    return NextResponse.redirect(new URL("/", req.nextUrl));
+  }
+  if (!publicPath && !token) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
+  }
+}
+
+export const config = {
+  matcher: ["/", "/login", "/dashboard"],
+};
