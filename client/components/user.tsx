4a0187f08cb732e1e01b90f3d78c2b22ed14fc0f
aziz-codes <azizisoffline@gmail.com>
2024-05-23 23:49:06 +0500

    Logged in user info displayed

--- client/components/user.tsx
+++ client/components/user.tsx
@@ -1,16 +1,21 @@
 import React from "react";
+import { auth } from "@/auth";
+
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-const user = () => {
+const user = async () => {
+  const session = await auth();
+
+  const user = session?.user;
   return (
     <div className="w-full flex justify-center lg:justify-start lg:border shadow-md rounded-md px-1 py-3 hover:scale-105 transition-transform duration-150 ease-linear cursor-pointer">
       <div className="flex gap-2 items-center">
         <Avatar className="h-8 w-8">
-          <AvatarFallback>AZ</AvatarFallback>
-          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>{user?.name?.charAt(0)}</AvatarFallback>
+          <AvatarImage src={user?.image as string} />
         </Avatar>
         <div className="flex-col -space-y-1 hidden lg:flex">
-          <h4 className="text-sm font-semibold">Aziz Khan</h4>
-          <p className="text-xs text-gray-400">@azizcodes</p>
+          <h4 className="text-sm font-semibold">{user?.name as string}</h4>
+          <p className="text-xs text-gray-400">{user?.username as string}</p>
         </div>
       </div>
     </div>
