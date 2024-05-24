1e439942c762dfa9616df5b6209146ef62a8421c
aziz-codes <azizisoffline@gmail.com>
2024-05-24 23:34:52 +0500

    new login page

---
+++ client/app/login/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <Button
+        onClick={() =>
+          signIn("github", {
+            callbackUrl: "http://localhost:3000",
+          })
+        }
+      >
+        Login here
+      </Button>
+    </div>
+  );
+};
+
+export default page;
