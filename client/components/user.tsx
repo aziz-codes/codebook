c09585c69f96194d089642d9ff841a49141dcecb
aziz-codes <azizisoffline@gmail.com>
2024-05-26 01:37:21 +0500

    re-useable user component

--- client/components/user.tsx
+++ client/components/user.tsx
@@ -2,20 +2,45 @@ import React from "react";
 import { auth } from "@/auth";
 
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-const user = async () => {
+
+interface Props {
+  hasBorder?: boolean;
+  hoverEffect?: boolean;
+  date?: string;
+}
+const user = async ({
+  hasBorder = true,
+  hoverEffect = true,
+  date = "",
+}: Props) => {
   const session = await auth();
 
   const user = session?.user;
   return (
-    <div className="w-full flex justify-center lg:justify-start lg:border shadow-md rounded-md px-1 py-3 hover:scale-105 transition-transform duration-150 ease-linear cursor-pointer">
+    <div
+      className={`w-full flex ${
+        date ? "justify-start " : "justify-center lg:justify-start"
+      }    shadow-md rounded-md px-1 ${
+        hoverEffect &&
+        "hover:scale-105 transition-transform duration-150 ease-linear"
+      } cursor-pointer ${hasBorder && "lg:border py-3"}`}
+    >
       <div className="flex gap-2 items-center">
         <Avatar className="h-8 w-8">
           <AvatarFallback>{user?.name?.charAt(0)}</AvatarFallback>
           <AvatarImage src={user?.image as string} />
         </Avatar>
-        <div className="flex-col -space-y-1 hidden lg:flex">
+        <div
+          className={`${
+            date
+              ? "flex flex-col -space-y-0.5"
+              : "flex-col -space-y-1.5 hidden lg:flex"
+          }`}
+        >
           <h4 className="text-sm font-semibold">{user?.name as string}</h4>
-          <p className="text-xs text-gray-400">{user?.username as string}</p>
+          <p className="text-xs text-gray-400">
+            {date ? date : (user?.username as string)}
+          </p>
         </div>
       </div>
     </div>
