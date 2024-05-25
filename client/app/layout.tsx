f7b5785771367e8d00e7b8d76623e6eea8866599
aziz-codes <azizisoffline@gmail.com>
2024-05-25 16:06:42 +0500

    code refactored

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "CodeBook",
-  description: "Developers Only Platfrom",
+  description: "Developers Only Platform",
 };
 
 export default async function RootLayout({
@@ -22,7 +22,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <body className={`${inter.className} `}>
+      <body className={inter.className}>
         <Provider>
           <ThemeProvider
             attribute="class"
@@ -30,25 +30,27 @@ export default async function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            {session && (
-              <nav className="fixed bottom-[calc(100vh-theme(spacing.12))] left-0 right-0 top-0 z-50">
-                <Navbar />
-              </nav>
-            )}
-
-            <div className="flex min-h-screen gap-10">
+            <div className="flex min-h-screen flex-col">
               {session && (
-                <aside className="sticky top-12 h-[calc(100vh-theme(spacing.12))] md:w-16 lg:w-56 overflow-y-auto border-r  hidden md:flex">
-                  <Sidebar />
-                </aside>
+                <nav className="fixed bottom-[calc(100vh-theme(spacing.12))] left-0 right-0 top-0 z-50">
+                  <Navbar />
+                </nav>
               )}
 
-              <main className="mt-12 flex flex-1 ">
-                <div className="flex-1 w-full py-4 px-4">{children}</div>
-                {/* <div className="w-44 border border-red-500 hidden md:flex">
-              <div className="fixed right-0">right bar</div>
-            </div> */}
-              </main>
+              <div className="flex flex-1 gap-10">
+                {session && (
+                  <aside className="sticky top-12 h-[calc(100vh-theme(spacing.12))] md:w-16 lg:w-56 overflow-y-auto border-r hidden md:flex">
+                    <Sidebar />
+                  </aside>
+                )}
+
+                <main className="mt-12 flex flex-1">
+                  <div className="flex-1 w-full py-4 px-4">{children}</div>
+                  {/* <div className="w-44 border border-red-500 hidden md:flex">
+                    <div className="fixed right-0">right bar</div>
+                  </div> */}
+                </main>
+              </div>
             </div>
           </ThemeProvider>
         </Provider>
@@ -56,4 +58,3 @@ export default async function RootLayout({
     </html>
   );
 }
-// navbar done here.
