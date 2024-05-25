fe2d05da16b7b57e53e264fb0ae83c8586d56959
aziz-codes <azizisoffline@gmail.com>
2024-05-26 01:37:32 +0500

    code highlighter component

---
+++ client/components/syntax-highlighter.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { dracula } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
+interface CodeProps {
+  language: string;
+  code: string;
+}
+
+const CodeHighlighter = ({ language, code }: CodeProps) => {
+  return (
+    <SyntaxHighlighter
+      customStyle={{
+        padding: "20px 5px",
+
+        height: "350px",
+        maxHeight: "350px",
+        msOverflowY: "auto",
+        borderRadius: "0px",
+        width: "100%",
+        overflow: "hidden",
+        overflowY: "auto",
+      }}
+      language={language}
+      style={dracula}
+      lineNumberStyle={dracula}
+      wrapLongLines={true}
+      showLineNumbers={true}
+      wrapLines={false}
+    >
+      {code}
+    </SyntaxHighlighter>
+  );
+};
+
+export default CodeHighlighter;
