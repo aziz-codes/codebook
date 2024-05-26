446be8727f9746db38e1416156a790d9efaaa608
aziz-codes <azizisoffline@gmail.com>
2024-05-26 18:25:44 +0500

    snippets route

--- client/app/snippets/page.tsx
+++ client/app/snippets/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Snippet from "@/components/snippet";
 const Snippets = () => {
   return (
-    <div className="w-full max-w-md sm:max-w-lg md:max-w-full">
+    <div className="w-full mx-auto md:mx-0 max-w-md sm:max-w-lg md:max-w-full">
       <Snippet />
       <Snippet />
     </div>
