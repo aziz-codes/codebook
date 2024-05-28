7595cb9bba0aee4a9d17f52b44f819ca53c60658
aziz-codes <azizisoffline@gmail.com>
2024-05-28 23:54:02 +0500

    tabs layout

---
+++ client/components/layouts/tabs.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserProfileTabs = () => {
+  return (
+    <div className="w-full max-w-5xl flex row mt-6 gap-8  ">
+      UserProfileTabs
+    </div>
+  );
+};
+
+export default UserProfileTabs;
