b96fcb40aa8ebae1b96b9ae3a19f4483f8a847c2
aziz-codes <azizisoffline@gmail.com>
2024-06-01 01:07:25 +0500

    user date profile component updated

--- client/components/layouts/user-date-profile.tsx
+++ client/components/layouts/user-date-profile.tsx
@@ -1,13 +1,18 @@
 import React from "react";
-import SingleAvatar from "../single-avatar";
+
 interface IAvatarProps {
   date: string;
   username: string;
+  image: string;
 }
-const UserDateProfile = ({ date, username }: IAvatarProps) => {
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+const UserDateProfile = ({ date, username, image }: IAvatarProps) => {
   return (
     <div className="flex items-center gap-1">
-      <SingleAvatar />
+      <Avatar>
+        <AvatarFallback>{username.slice(0, 2)}</AvatarFallback>
+        <AvatarImage src={image} />
+      </Avatar>
       <div className="flex flex-col -space-y-1">
         <h4 className="text-sm font-semibold">{username}</h4>
         <p className="text-xs text-gray-400">{date}</p>
