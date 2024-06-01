0b7c2ad0823f051d0e0aaed5f45516105bec1412
aziz-codes <azizisoffline@gmail.com>
2024-06-01 15:17:21 +0500

    layout navbar height

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -32,19 +32,19 @@ export default async function RootLayout({
           >
             <div className="flex min-h-screen flex-col">
               {session && (
-                <nav className="fixed bottom-[calc(100vh-theme(spacing.12))] left-0 right-0 top-0 z-50">
+                <nav className="fixed bottom-[calc(100vh-theme(spacing.16))] left-0 right-0 top-0 z-50">
                   <Navbar />
                 </nav>
               )}
 
               <div className="flex flex-1 gap-10">
                 {session && (
-                  <aside className="sticky top-12 h-[calc(100vh-theme(spacing.12))] md:w-16 lg:w-56 overflow-y-auto border-r hidden md:flex">
+                  <aside className="sticky top-16 h-[calc(100vh-theme(spacing.16))] md:w-16 lg:w-56 overflow-y-auto border-r hidden md:flex">
                     <Sidebar />
                   </aside>
                 )}
 
-                <main className="mt-12 flex flex-1">
+                <main className="mt-16 flex flex-1">
                   <div className="flex-1 w-full py-4 px-4">{children}</div>
                   {/* <div className="w-44 border border-red-500 hidden md:flex">
                     <div className="fixed right-0">right bar</div>
