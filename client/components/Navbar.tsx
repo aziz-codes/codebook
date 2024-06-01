50400b9be39994c8a8aade7c0347fe41f5bf9861
aziz-codes <azizisoffline@gmail.com>
2024-06-01 20:55:01 +0500

    dark and light theme color

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -12,10 +12,11 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 import UserAvatar from "./user-avatar";
+import ThemeToggle from "./theme-toggle";
 
 const Navbar = () => {
   return (
-    <div className="relative h-full bg-background border-b  flex items-center px-4 justify-between ">
+    <div className="relative h-full bg-[--background] dark:bg-[#121212] border-b  flex items-center px-4 justify-between ">
       <div className="brand font-semibold">Test</div>
       {/* <div className="w-[50%]"></div> */}
       <div className="actionIcons flex items-center space-x-5">
@@ -23,7 +24,7 @@ const Navbar = () => {
           <DropdownMenuTrigger>
             <SearchIcon className="text-gray-400 h-5 w-5 cursor-pointer hover:scale-105 !ring-0 focus-within:!ring-0 transition-transform ease-in" />
           </DropdownMenuTrigger>
-          <DropdownMenuContent>
+          <DropdownMenuContent className="dark:bg-red-500 bg-green-500">
             <DropdownMenuLabel>My Account</DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuItem>Profile</DropdownMenuItem>
@@ -32,7 +33,7 @@ const Navbar = () => {
             <DropdownMenuItem>Subscription</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
-
+        <ThemeToggle />
         <Mail className="text-gray-400 h-5 w-5 cursor-pointer hover:scale-105 transition-transform ease-in" />
         <Bell className="text-gray-400 h-5 w-5 cursor-pointer hover:scale-105 transition-transform ease-in" />
 
