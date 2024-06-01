1e2fc9d07fc2a6ac43c30e2349238dba224ed135
aziz-codes <azizisoffline@gmail.com>
2024-06-01 17:48:11 +0500

    removed dark and light mode fun

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,8 +1,7 @@
 "use client";
 import React from "react";
-import { Bell, Mail, Moon, SearchIcon, Sun } from "lucide-react";
+import { Bell, Mail, SearchIcon } from "lucide-react";
 
-import { useTheme } from "next-themes";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -15,16 +14,14 @@ import {
 import UserAvatar from "./user-avatar";
 
 const Navbar = () => {
-  const { setTheme } = useTheme();
-
   return (
-    <div className="relative h-full bg-background border-b shadow flex items-center px-4 justify-between ">
+    <div className="relative h-full bg-background border-b  flex items-center px-4 justify-between ">
       <div className="brand font-semibold">Test</div>
-      <div className="w-[50%]"></div>
+      {/* <div className="w-[50%]"></div> */}
       <div className="actionIcons flex items-center space-x-5">
         <DropdownMenu>
           <DropdownMenuTrigger>
-            <SearchIcon className="text-gray-400 h-5 w-5 cursor-pointer hover:scale-105 transition-transform ease-in" />
+            <SearchIcon className="text-gray-400 h-5 w-5 cursor-pointer hover:scale-105 !ring-0 focus-within:!ring-0 transition-transform ease-in" />
           </DropdownMenuTrigger>
           <DropdownMenuContent>
             <DropdownMenuLabel>My Account</DropdownMenuLabel>
@@ -38,8 +35,7 @@ const Navbar = () => {
 
         <Mail className="text-gray-400 h-5 w-5 cursor-pointer hover:scale-105 transition-transform ease-in" />
         <Bell className="text-gray-400 h-5 w-5 cursor-pointer hover:scale-105 transition-transform ease-in" />
-        <Sun onClick={() => setTheme("light")} />
-        <Moon onClick={() => setTheme("dark")} />
+
         <UserAvatar />
       </div>
     </div>
