176070c2bcae4487f3c0e670e1a111e26db6d581
aziz-codes <azizisoffline@gmail.com>
2024-06-02 15:33:36 +0500

    profile componented refactored more

--- client/components/metro-profile.tsx
+++ client/components/metro-profile.tsx
@@ -3,10 +3,18 @@ import ProfileAvatar from "./profile-avatar";
 import { Button } from "./ui/button";
 import { Label } from "./ui/label";
 import { Separator } from "./ui/separator";
+import {
+  HomeIcon,
+  Users,
+  TrendingUp,
+  CodeXml,
+  BadgeCent,
+  UserCheck,
+} from "lucide-react";
 const MetroProfile = () => {
   return (
     <div
-      className={`w-full h-auto bg-[--background] dark:bg-semiDark rounded-md p-4 `}
+      className={`w-full h-auto bg-[--background] dark:bg-semiDark rounded-md py-12 px-6`}
     >
       <div className="flex flex-col md:flex-row gap-4 flex-wrap items-center justify-between">
         <div className="flex gap-4 ">
@@ -14,28 +22,54 @@ const MetroProfile = () => {
             username="azizcodes"
             image="https://github.com/shadcn.png"
           />
-          <div className="flex flex-col max-w-xs gap-3">
+          <div className="flex flex-col max-w-md gap-3">
             <div>
               <h4 className="font-bold">Aziz</h4>
-              <p className="line-clamp-2 text-sm font-[500]">
+
+              <p className="line-clamp-3 text-sm font-[500] text-gray-400">
                 Frontend web developer, talks about JS, React, Node, PHP, MYSQL
               </p>
             </div>
             <Separator />
-            <div className="flex items-center  max-w-xs  justify-between">
-              <div className="flex flex-col space-y-1 ">
+            <div className="flex items-center w-full max-w-md  justify-between">
+              <div className="flex flex-col space-y-1 " title="discussions">
                 <Label className="font-bold">25</Label>
-                <p className="text-gray-400  text-sm">Discussions</p>
+                <p className="text-gray-400  text-sm hidden md:block">
+                  Discussions
+                </p>
+                <TrendingUp className="block md:hidden" />
               </div>
               <Separator orientation="vertical" />
-              <div className="flex flex-col space-y-1 ">
+              <div className="flex flex-col space-y-1 " title="snippets">
                 <Label className="font-bold">25</Label>
-                <p className="text-gray-400  text-sm">Snippets</p>
+                <p className="text-gray-400  text-sm hidden md:block">
+                  Snippets
+                </p>
+                <CodeXml className="block md:hidden" />
               </div>
               <Separator orientation="vertical" />
-              <div className="flex flex-col space-y-1 ">
+              <div className="flex flex-col space-y-1 " title="bounties">
                 <Label className="font-bold">25</Label>
-                <p className="text-gray-400  text-sm">Bounties</p>
+                <p className="text-gray-400  text-sm hidden md:block">
+                  Bounties
+                </p>
+                <BadgeCent className="block md:hidden" />
+              </div>
+              <Separator orientation="vertical" />
+              <div className="flex flex-col space-y-1 " title="followers">
+                <Label className="font-bold">25</Label>
+                <p className="text-gray-400  text-sm hidden md:block">
+                  Followers
+                </p>
+                <Users className="block md:hidden" />
+              </div>
+              <Separator orientation="vertical" />
+              <div className="flex flex-col space-y-1 " title="Following">
+                <Label className="font-bold">25</Label>
+                <p className="text-gray-400  text-sm hidden md:block">
+                  Follwing
+                </p>
+                <UserCheck className="block md:hidden" />
               </div>
             </div>
           </div>
