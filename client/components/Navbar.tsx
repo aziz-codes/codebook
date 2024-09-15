18d6b6d2478b75b4d829e2383a5898af61829eb3
aziz-codes <azizisoffline@gmail.com>
2024-09-15 19:40:14 +0500

    client/components/Navbar.tsx

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,15 +1,7 @@
 "use client";
 import React from "react";
-import { Bell, Mail, SearchIcon } from "lucide-react";
+import { Bell, Mail } from "lucide-react";
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 
 import UserAvatar from "./user-avatar";
 import ThemeToggle from "./theme-toggle";
@@ -18,22 +10,10 @@ const Navbar = () => {
   return (
     <div className="relative h-full bg-[--background] dark:bg-[#121212]   flex items-center px-4 justify-between ">
       <div className="brand font-semibold">Test</div>
-      {/* <div className="w-[50%]"></div> */}
+
       <div className="actionIcons flex items-center space-x-5">
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            <SearchIcon className="text-gray-400 h-5 w-5 cursor-pointer hover:scale-105 !ring-0 focus-within:!ring-0 transition-transform ease-in" />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="dark:bg-red-500 bg-green-500">
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>Billing</DropdownMenuItem>
-            <DropdownMenuItem>Team</DropdownMenuItem>
-            <DropdownMenuItem>Subscription</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-        <ThemeToggle />
+
+
         <Mail className="text-gray-400 h-5 w-5 cursor-pointer hover:scale-105 transition-transform ease-in" />
         <Bell className="text-gray-400 h-5 w-5 cursor-pointer hover:scale-105 transition-transform ease-in" />
 
