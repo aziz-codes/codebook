f80475ca5387ec82ca8514d3fb34bd13190a1128
aziz-codes <azizisoffline@gmail.com>
2024-09-15 23:20:29 +0500

    removed some links from navbar

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -10,10 +10,7 @@ const Navbar = () => {
   return (
     <div className="relative h-full bg-[--background] dark:bg-[#121212]   flex items-center px-4 justify-between ">
       <div className="brand font-semibold">Test</div>
-
       <div className="actionIcons flex items-center space-x-5">
-
-
         <Mail className="text-gray-400 h-5 w-5 cursor-pointer hover:scale-105 transition-transform ease-in" />
         <Bell className="text-gray-400 h-5 w-5 cursor-pointer hover:scale-105 transition-transform ease-in" />
 
