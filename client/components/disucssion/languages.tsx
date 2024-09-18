430ac3a313f3be52ec6046fbcb6a687061b15c8f
aziz-codes <azizisoffline@gmail.com>
2024-09-19 01:21:48 +0500

    count added

--- client/components/disucssion/languages.tsx
+++ client/components/disucssion/languages.tsx
@@ -4,9 +4,9 @@ const Lanuages = () => {
   return (
     <>
     {languages.map((language,index)=>(
-        <div className='flex items-center' key={index}>
+        <div className='flex items-center justify-between px-2 text-xs hover:bg-bgHover py-1.5 rounded-md cursor-pointer' key={index}>
         
-            <label>{language.name}</label>
+            <label>{language.name}</label> <span>{language.count}</span>
         </div>
     ))}
     </>
