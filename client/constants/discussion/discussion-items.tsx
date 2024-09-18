b92023ce72e7b10f7ae6dd7aa125447dc755c293
aziz-codes <azizisoffline@gmail.com>
2024-09-19 01:22:12 +0500

    discussion items

--- client/constants/discussion/discussion-items.tsx
+++ client/constants/discussion/discussion-items.tsx
@@ -1,53 +1,51 @@
-import React from "react";
-
 interface Language {
   name: string;
- 
+  count: number;
 }
 
 export const languages: Language[] = [
   {
     name: "JavaScript",
-     
+    count: 14, // Example count for JavaScript
   },
   {
     name: "Python",
-   
+    count: 22,
   },
   {
     name: "Java",
-    
+    count: 18,
   },
   {
     name: "TypeScript",
-     
+    count: 9,
   },
   {
     name: "C++",
-     
+    count: 11,
   },
   {
     name: "C#",
-    
+    count: 16,
   },
   {
     name: "Go",
-     
+    count: 7,
   },
   {
     name: "Ruby",
-    
+    count: 5,
   },
   {
     name: "PHP",
-     
+    count: 12,
   },
   {
     name: "Swift",
-    
+    count: 6,
   },
   {
     name: "Kotlin",
-     
+    count: 4,
   },
 ];
