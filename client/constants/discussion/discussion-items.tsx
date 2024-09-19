46265aa73c9a77c1287795dbf85e265c43e5a176
aziz-codes <azizisoffline@gmail.com>
2024-09-19 22:30:58 +0500

    discussion array list

--- client/constants/discussion/discussion-items.tsx
+++ client/constants/discussion/discussion-items.tsx
@@ -1,3 +1,4 @@
+import { users } from "@/constants/users";
 interface Language {
   name: string;
   count: number;
@@ -47,5 +48,35 @@ export const languages: Language[] = [
   {
     name: "Kotlin",
     count: 4,
+  },
+];
+
+export const discussions = [
+  {
+    avatar: users[0].avatar,
+    description:
+      "i having an issue with streaming my camera rtsp using node server and react in the frontend",
+    tags: ["node js", "react", "webrtc"],
+    comments: 12,
+    date: "14 days ago",
+    isAnswered: true,
+  },
+  {
+    avatar: users[1].avatar,
+    description:
+      "getting cors policy error while accessing server localhost:50000",
+    tags: ["node js", "express", "frontend"],
+    comments: 31,
+    date: "1 days ago",
+    isAnswered: false,
+  },
+  {
+    avatar: users[3].avatar,
+    description:
+      "how to configure docker in windows?",
+    tags: ["docker", "windows", "suggestion"],
+    comments: 45,
+    date: "01 month ago",
+    isAnswered: true,
   },
 ];
