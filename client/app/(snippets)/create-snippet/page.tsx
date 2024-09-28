a781c781baa35bfa53c51c6a98c501c1db9d433d
aziz-codes <azizisoffline@gmail.com>
2024-09-29 02:04:42 +0500

    create snippet route

---
+++ client/app/(snippets)/create-snippet/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {default as Snippet}  from '@/components/snippets/create-snippet'
+const CreateSnippet = () => {
+  return (
+    <Snippet />
+  )
+}
+
+export default CreateSnippet
