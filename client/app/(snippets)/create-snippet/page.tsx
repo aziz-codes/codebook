355724f668fc91c784d16ebb260254d971c54a17
aziz-codes <azizisoffline@gmail.com>
2024-09-30 22:52:10 +0500

    create snippet route wrapped by main wrapper

--- client/app/(snippets)/create-snippet/page.tsx
+++ client/app/(snippets)/create-snippet/page.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import {default as Snippet}  from '@/components/snippets/create-snippet'
+import MainWrapper from '@/layouts/main-wrapper'
+import { childRoutesClass } from '@/utilities'
 const CreateSnippet = () => {
   return (
-    <Snippet />
+    <MainWrapper classes={childRoutesClass}>
+
+      <Snippet />
+    </MainWrapper>
   )
 }
 
