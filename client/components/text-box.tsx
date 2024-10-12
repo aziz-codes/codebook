87fcd38b36d6b6636af0ccb5c9ce18debf4e0299
aziz-codes <azizisoffline@gmail.com>
2024-10-13 03:06:49 +0500

    added post button to textbox

--- client/components/text-box.tsx
+++ client/components/text-box.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { Smile } from 'lucide-react';
 import React, { useEffect, useRef, useState } from 'react'
+import { Button } from './ui/button';
 
 const TextBox = () => {
     const [input, setInput] = useState("");  
@@ -19,16 +20,19 @@ const TextBox = () => {
       adjustHeight();  
     }, [input]);
   return (
-    <div className="flex justify-between  px-1 items-center w-full ">
+    <div className="flex justify-between px-1 items-center w-full ">
       <textarea
         placeholder="Add a comment"
         ref={inputRef}
         className="resize-none overflow-hidden w-full p-2 outline-none bg-transparent"
         value={input}
-        onChange={(e) => setInput(e.target.value)} // Set input state on change
-        style={{ height: "34px" }} // Initial height
+        onChange={(e) => setInput(e.target.value)}  
+        style={{ height: "34px" }}  
       ></textarea>
-         <Smile className="cursor-pointer hover:scale-105 transition-all duration-75 ease-linear hover:text-gray-400" />
+    <div className='flex items-center gap-1'>
+    {input.length > 0 && <Button variant="link" className='!no-underline text-sky-600  p-0 px-1'>Post</Button>}
+    <Smile className="h-5 w-5 cursor-pointer hover:scale-105 transition-all duration-75 ease-linear hover:text-gray-400" />
+    </div>
     </div>
   )
 }
