102c7c9974475b42adbf174b9d1f365b34196e03
aziz-codes <azizisoffline@gmail.com>
2024-10-14 23:05:21 +0500

    adding a create component to home route

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,22 +3,25 @@ import Post from "@/components/Post";
 import { auth } from "@/auth";
 import MainWrapper from "@/layouts/main-wrapper";
 import { topMargin } from "@/utilities";
+import Create from "@/components/create";
 const HomePage = async () => {
   const session = await auth();
-
+  
   return (
-    <MainWrapper classes="w-full  ">
-      <div className={`w-full max-w-lg mt-${topMargin}`}>
-        {!session?.user ? (
-          "No session"
-        ) : (
-          <>
-            <Post />
-            <Post />
-            <Post />
-          </>
-        )}
-      </div>
+    <MainWrapper classes="w-full">
+ <div className={`w-full flex justify-between gap-4 border border-red-500 mt-${topMargin}`}>
+  {/* Left side content */}
+  <div className="flex flex-col gap-4 w-8/12">
+    <Create />
+    <Post />
+    <Post />
+    <Post />
+  </div>
+
+  {/* Right side content */}
+  <div className="w-4/12 border">right side</div>
+</div>
+
     </MainWrapper>
   );
 };
