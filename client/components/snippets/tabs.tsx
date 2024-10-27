0735a59c54f83402915ffdd0d45a0b3d643ded86
aziz-codes <azizisoffline@gmail.com>
2024-10-27 23:59:08 +0500

    snippet tabs component

--- client/components/snippets/tabs.tsx
+++ client/components/snippets/tabs.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
-const Tabs = () => {
+const SnippetTab = () => {
   return (
-    <div>Tabs</div>
+    <Tabs defaultValue="for-you" className="w-full p-0">
+    <TabsList className='w-full p-0 border-2 '>
+      <TabsTrigger value="for-you" className='flex-1 h-full m-0 rounded-md'>For You</TabsTrigger>
+      <TabsTrigger value="following">Following</TabsTrigger>
+    </TabsList>
+    <TabsContent value="for-you" className='flex-1'>For you Tab</TabsContent>
+    <TabsContent value="following">Following Tab</TabsContent>
+  </Tabs>
+  
   )
 }
 
-export default Tabs
+export default SnippetTab
