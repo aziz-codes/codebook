5059159218e79ba2ddb31997e8e7e345e97272a8
aziz-codes <azizisoffline@gmail.com>
2024-10-27 23:46:05 +0500

    added more lagnuages

--- client/components/disucssion/languages.tsx
+++ client/components/disucssion/languages.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import {languages} from '@/constants/discussion/discussion-items'
 const Lanuages = () => {
   return (
-    <>
+    <div className='h-[75vh] overflow-y-auto px-2'>
     {languages.map((language,index)=>(
-        <div className='flex items-center justify-between px-2 text-xs hover:bg-bgHover py-1.5 rounded-md cursor-pointer' key={index}>
+        <div className='flex items-center justify-between px-2 text-xs hover:bg-bgHover py-1.5  rounded-md cursor-pointer pr-4' key={index}>
         
             <label>{language.name}</label> <span>{language.count}</span>
         </div>
     ))}
-    </>
+    </div>
   )
 }
 
