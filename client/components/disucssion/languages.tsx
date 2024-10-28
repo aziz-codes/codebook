6afd90d20929bd0f0105471a6a5fc28fd97eab21
aziz-codes <azizisoffline@gmail.com>
2024-10-28 23:47:31 +0500

    fixed allignment issue

--- client/components/disucssion/languages.tsx
+++ client/components/disucssion/languages.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {languages} from '@/constants/discussion/discussion-items'
 const Lanuages = () => {
   return (
-    <div className='h-[75vh] overflow-y-auto px-2'>
+    <div className='h-[75vh] overflow-y-auto pr-2'>
     {languages.map((language,index)=>(
         <div className='flex items-center justify-between px-2 text-xs hover:bg-bgHover py-1.5  rounded-md cursor-pointer pr-4' key={index}>
         
