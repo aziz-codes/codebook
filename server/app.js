744c2428fb605a1fccd05e037cee855d7d3f0152
aziz-codes <azizisoffline@gmail.com>
2024-10-30 23:34:55 +0500

    basic node and express server

--- server/app.js
+++ server/app.js
@@ -1,5 +1,16 @@
 import dotenv from 'dotenv';
 import express from 'express'
+import snippetRoutes from './routes/snippets-routes.js';
+
 dotenv.config();
 const url = process.env.MONGO_URI;
-console.log('url is ',url);
+
+const app = express();
+app.use(express.json())
+app.use("/snippets",snippetRoutes)
+const port = 8000;
+
+app.listen(port,()=>{
+    console.log(`server is running on port:${port}`)
+})
+ 
