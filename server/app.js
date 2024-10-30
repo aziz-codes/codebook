246eb4144c5573615574b6297ac32058f20b9b81
aziz-codes <azizisoffline@gmail.com>
2024-10-31 00:51:06 +0500

    user route included

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express'
 import snippetRoutes from './routes/snippets-routes.js';
+import userRoute from './routes/user-routes.js';
+import mongoose from 'mongoose';
 
 dotenv.config();
 const url = process.env.MONGO_URI;
@@ -8,9 +10,18 @@ const url = process.env.MONGO_URI;
 const app = express();
 app.use(express.json())
 app.use("/snippets",snippetRoutes)
+app.use("/user",userRoute);
 const port = 8000;
-
 app.listen(port,()=>{
     console.log(`server is running on port:${port}`)
 })
+// mongodb connection
+try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected successfully");
+} catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1); // Exit the process with failure
+}
+
  
