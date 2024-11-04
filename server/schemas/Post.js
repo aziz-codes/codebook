2784cf3662b08e7c867cb5b41e36978e11ba76da
aziz-codes <azizisoffline@gmail.com>
2024-11-05 00:07:02 +0500

    schemas for post

---
+++ server/schemas/Post.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+const PostSchema = new mongoose.Schema(
+    {
+      user:{
+        type: mongoose.Types.ObjectId,
+        ref: 'user',
+        required: true
+      },
+      title:{
+        type: String,
+        required: true
+      },
+    },
+    {timestamps: true}
+)
+
+const Post = mongoose.models.Post || mongoose.model('Post',PostSchema);
+export default Post;
