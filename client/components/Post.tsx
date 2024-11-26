6668975f3b6c6d67fe2dcbc5c1366f220ca96883
aziz-codes <azizisoffline@gmail.com>
2024-11-27 01:58:27 +0500

    refactored.

--- client/components/Post.tsx
+++ client/components/Post.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { FC, useEffect, useRef, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Ellipsis } from "lucide-react";
 import TimeAgo from "react-timeago";
