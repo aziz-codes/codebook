1d85d1e980136c0f9231ed5dac7d8d68faca182c
aziz-codes <azizisoffline@gmail.com>
2024-11-28 03:57:52 +0500

    checking new user wile routing

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,20 +1,39 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse, NextRequest } from "next/server";
-
-export default async function middlware(req: NextRequest) {
+ 
+export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
+ 
   const token = await getToken({
     req: req,
     secret: process.env.NEXTAUTH_SECRET,
   });
+
+  console.log(token?.isNewUser);  
+
   const publicPath = path === "/login";
 
+ 
   if (publicPath && token) {
-    return NextResponse.redirect(new URL("/", req.nextUrl));
+  
+     
+     
+      return NextResponse.redirect(new URL("/", req.nextUrl));
+     
   }
+ 
+
+  
   if (!publicPath && !token) {
     return NextResponse.redirect(new URL("/login", req.nextUrl));
   }
+
+  if (token?.isNewUser) {
+    console.log("Redirecting to new-user setup page");
+    return NextResponse.redirect(new URL("/new-user", req.nextUrl));
+  }
+
+  return NextResponse.next();  
 }
 
 export const config = {
