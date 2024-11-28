93dceb957045b7538a736093ef71d02149764ca9
aziz-codes <azizisoffline@gmail.com>
2024-11-28 20:15:18 +0500

    middleware refactored

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,39 +1,32 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse, NextRequest } from "next/server";
- 
+
 export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
- 
+
   const token = await getToken({
     req: req,
     secret: process.env.NEXTAUTH_SECRET,
   });
 
-  console.log(token?.isNewUser);  
+  console.log(token?.isNewUser);
 
   const publicPath = path === "/login";
 
- 
   if (publicPath && token) {
-  
-     
-     
-      return NextResponse.redirect(new URL("/", req.nextUrl));
-     
+    return NextResponse.redirect(new URL("/", req.nextUrl));
   }
- 
 
-  
   if (!publicPath && !token) {
     return NextResponse.redirect(new URL("/login", req.nextUrl));
   }
 
   if (token?.isNewUser) {
-    console.log("Redirecting to new-user setup page");
+  
     return NextResponse.redirect(new URL("/new-user", req.nextUrl));
   }
 
-  return NextResponse.next();  
+  return NextResponse.next();
 }
 
 export const config = {
@@ -44,6 +37,6 @@ export const config = {
     "/bounties",
     "/snippets",
     "/peoples",
-    "/create-snippet"
+    "/create-snippet",
   ],
 };
