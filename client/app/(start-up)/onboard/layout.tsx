f8395f78ad6286c9b426c93f41b216dcbe240a32
aziz-codes <azizisoffline@gmail.com>
2024-11-30 03:55:56 +0500

    onboard rollout

---
+++ client/app/(start-up)/onboard/layout.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React from "react";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { useSession } from "next-auth/react";
+ 
+const NewUserLayout = ({ children }: { children: React.ReactNode }) => {
+  
+  const {data:session,status} = useSession();
+  if(status === "loading"){
+    return <h4>Loading session pelase wait</h4>
+  }
+
+  if(status === "authenticated" && session.user.isOnboarded){
+   window.location.href="/"
+    return null;
+  }
+  
+  return (
+    <Dialog open>
+    <DialogContent className="p-0 py-6 max-h-[90vh] overflow-y-auto max-w-sm !ring-0">
+      <DialogHeader className="text-center p-0 !px-4">
+        <DialogTitle className="py-2">Accounts to Follow</DialogTitle>
+      </DialogHeader>
+      <div className="px-4">
+        {children}
+      </div>
+    </DialogContent>
+  </Dialog>
+  );
+};
+
+export default NewUserLayout;
