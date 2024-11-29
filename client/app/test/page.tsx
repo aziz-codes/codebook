f6c3aa74bf5c03a7c4b5088feab2080c32f71df0
aziz-codes <azizisoffline@gmail.com>
2024-11-30 03:57:04 +0500

    test user cards

--- client/app/test/page.tsx
+++ client/app/test/page.tsx
@@ -1,62 +1,21 @@
-"use client";
-import data from "@emoji-mart/data";
-import Picker from "@emoji-mart/react";
-import { useState } from "react";
+"use client"
+import UserCard from '@/components/uers/card'
+import { useSession } from 'next-auth/react'
+import React from 'react'
 
 const Test = () => {
-  const [open, setOpen] = useState(false);
-  const [emojis, setEmojis] = useState<string[]>([]);
-
-  const handleEmojiSelect = (item: any) => {
-    setEmojis((prev) => [...prev, item.native]);
-  };
-
+  const {data:session} = useSession();
   return (
-    <div>
-      <button onClick={() => setOpen((prev) => !prev)}>
-        {open ? "Close" : "Open"}
-      </button>
-      <div style={{ marginTop: "10px", fontSize: "24px" }}>
-        {/* Display selected emojis */}
-        {emojis.map((emoji, index) => (
-          <span key={index} style={{ marginRight: "5px" }}>
-            {emoji}
-          </span>
-        ))}
-      </div>
+    <div className="flex flex-wrap gap-3">
+  <UserCard name="Aziz" username="azizcodes" avatar={session?.user.image as string} _id="" />
+  <UserCard name="Aziz" username="azizcodes" avatar={session?.user.image as string} _id="" />
+  <UserCard name="Aziz" username="azizcodes" avatar={session?.user.image as string} _id="" />
+  <UserCard name="Aziz" username="azizcodes" avatar={session?.user.image as string} _id="" />
+  <UserCard name="Aziz" username="azizcodes" avatar={session?.user.image as string} _id="" />
+  {/* Add more UserCard components as needed */}
+</div>
 
-      {open && (
-        <div style={{ position: "relative" }}>
-          {/* Custom close button */}
-          <button
-            onClick={() => setOpen(false)}
-            style={{
-              position: "absolute",
-              top: 10,
-              right: 10,
-              background: "transparent",
-              border: "none",
-              fontSize: "20px",
-              cursor: "pointer",
-            }}
-          >
-            ✖
-          </button>
+  )
+}
 
-          <Picker
-          navPosition="none"
-            data={data}
-            onEmojiSelect={handleEmojiSelect}
-            previewPosition="none"
-            onClickOutside={() => setOpen(false)}
-             className="border"
-            emojiSize={12}  
-            emojiButtonSize={20}  
-          />
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default Test;
+export default Test
