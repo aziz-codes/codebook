4c30c3770b152180091b0474354e369ff1ee7750
aziz-codes <azizisoffline@gmail.com>
2024-11-29 23:17:48 +0500

    UI updates added following & followers

--- client/test/profile-card.tsx
+++ client/test/profile-card.tsx
@@ -1,15 +1,17 @@
 "use client";
 import React from "react";
- 
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import {  UserProfileType } from "@/types/user";
+import { useSession } from "next-auth/react";
+import { UserProfileType } from "@/types/user";
+import { UsersRound } from "lucide-react";
 type ProfileCardProps = {
-  profile: UserProfileType;  
+  profile: UserProfileType;
 };
-const ProfileCard:React.FC<ProfileCardProps> = ({profile:user}) => {
-
+const ProfileCard: React.FC<ProfileCardProps> = ({ profile: user }) => {
+  const { data: session } = useSession();
   const stats = [
     {
       name: "Posts",
@@ -28,10 +30,10 @@ const ProfileCard:React.FC<ProfileCardProps> = ({profile:user}) => {
       count: "12/4",
     },
   ];
-  console.log(user.user.name)
+  console.log(user.user.name);
   return (
-    <div className="bg-bgCard w-full rounded-md px-4 py-4 grid grid-cols-12 gap-4">
-      <div className="col-span-12 md:col-span-4 flex flex-col  p-4 justify-center items-center">
+    <div className="bg-bgCard w-full rounded-md px-4 py-4 grid grid-cols-12 gap-2">
+      <div className="col-span-12 md:col-span-3 flex flex-col  p-4 justify-center items-center">
         <Avatar className="rounded-full md:rounded-md h-44 w-44">
           <AvatarFallback>
             {user.user.name?.charAt(0)}
@@ -40,11 +42,23 @@ const ProfileCard:React.FC<ProfileCardProps> = ({profile:user}) => {
           <AvatarImage src={user.user.avatar} />
         </Avatar>
         <div className="py-3 text-center">
-          <Label className="text-center text-lg capitalize">
+          <Label className="text-center text-sm font-semibold capitalize">
             {user.user.name}
           </Label>
-          <p className="text-sm">{user.user.username}</p>
-          <Label className="font-[400]">{user.user.tagline}</Label>
+          {/* <p className="text-sm">{user.user.username}</p> */}
+          {/* <Label className="font-[400]">{user.user.tagline}</Label> */}
+          <div className="flex items-center gap-1 mt-2">
+          <UsersRound className="h-4 w-4 text-gray-400"/>
+           <div className="flex text-xs space-x-1">
+            <h4 className="font-semibold">3</h4>
+            <p className="text-gray-400 text-xs">Followers</p>
+           </div>
+
+           <div className="flex text-xs space-x-1">
+            <h4 className="font-semibold">8</h4>
+            <p className="text-gray-400 text-xs">Following</p>
+           </div>
+        </div>
         </div>
       </div>
       <div className="col-span-12  md:col-span-7  overflow-hidden  p-4 flex flex-col gap-5 items-center md:items-start">
@@ -57,16 +71,30 @@ const ProfileCard:React.FC<ProfileCardProps> = ({profile:user}) => {
           ))}
         </div>
         {/* BIO */}
-        <Label className="line-clamp-3 font-normal leading-5 max-w-xs">
-        {user.user.bio}
-        </Label>
+       
+        <div>
+        <p className="text-sm font-semibold text-gray-400">{user.user.username}</p>
+        <Label className="font-[400] ">{user.user.tagline}</Label>
+         <Label
+          className="line-clamp-3 font-normal leading-5 max-w-xs text-xs py-2"
+          dangerouslySetInnerHTML={{
+            __html: user.user.bio.replace(/\n/g, "<br>").slice(0,165),
+          }}
+        />
+        </div>
         <div className="flex items-center  space-x-4 max-w-xs">
-          <Button size="sm" variant="secondary" className="flex-1">
-            Following
-          </Button>
-          <Button size="sm" variant="secondary" className="flex-1">
-            Message
-          </Button>
+          {user.user.username === session?.user.username ? (
+            <button className="px-8 py-1.5 rouneded-md bg-bgHover rounded-md">edit profile</button>
+          ) : (
+            <>
+              <Button size="sm" variant="secondary" className="flex-1">
+                Follow
+              </Button>
+              <Button size="sm" variant="secondary" className="flex-1">
+                Message
+              </Button>
+            </>
+          )}
         </div>
       </div>
     </div>
