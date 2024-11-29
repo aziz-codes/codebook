ba5be6c92e8e446fa24d8983a7c8bbdcee3ae24d
aziz-codes <azizisoffline@gmail.com>
2024-11-29 05:09:46 +0500

    button loader

---
+++ client/utils/components/button-loader.tsx
@@ -0,0 +1,8 @@
+const ButtonLoader = () => {
+    return (
+      <div className='h-4 w-4 border-2 border-solid border-t-sky-500 border-r-sky-500 border-transparent rounded-full animate-spin'></div>
+    )
+  }
+  
+  export default ButtonLoader;
+  
