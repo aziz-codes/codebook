b55f96a1eb0a6993c7f5b71e381c57d06a075eb9
aziz-codes <azizisoffline@gmail.com>
2024-12-01 23:57:26 +0500

    post controller renamed

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,8 +1,8 @@
 import express from 'express';
 
  
-import { deletePost, getPosts, getSinglePost, post } from '../controllers/post-controllers.js';
-import { toggleLikePost } from '../controllers/like-controller.js';
+import { deletePost, getPosts, post } from '../controllers/post.controller.js';
+import { toggleLikePost } from '../controllers/like.controller.js';
 const router = express.Router();
 
 
