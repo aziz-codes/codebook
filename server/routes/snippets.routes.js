730aa49dc31ad706ba69595348e6cd5734b37a82
aziz-codes <azizisoffline@gmail.com>
2024-12-01 23:57:42 +0500

    snippet controller renamed

--- server/routes/snippets.routes.js
+++ server/routes/snippets.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { allSnippets, saveSnippet } from '../controllers/snippet-controllers.js';
+import { allSnippets, saveSnippet } from '../controllers/snippet.controller.js';
 
 
 const router = express.Router();
