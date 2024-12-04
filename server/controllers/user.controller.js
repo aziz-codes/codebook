9307644d2c3d2aab11a0b098a0ffbeb7494cea81
aziz-codes <azizisoffline@gmail.com>
2024-12-04 23:39:19 +0500

    removed console log

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -65,7 +65,7 @@ export const getUser = async (req, res) => {
       followers,
       following
     });
-    console.log(user)
+ 
   } catch (error) {
     console.error("Error fetching user data:", error);
     res.status(500).json({ error: "Could not fetch user data." });
