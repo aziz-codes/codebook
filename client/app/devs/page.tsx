1106f572de38e4971a991f1026ef040630d2fe5a
aziz-codes <azizisoffline@gmail.com>
2024-12-04 23:06:33 +0500

    fetching all users on server side

--- client/app/devs/page.tsx
+++ client/app/devs/page.tsx
@@ -1,6 +1,27 @@
+import DevCard from "@/components/uers/dev-card";
 import MainWrapper from "@/layouts/main-wrapper";
-const page = () => {
-  return <MainWrapper classes="flex flex-col">dev cards</MainWrapper>;
+import { getRequest } from "@/services";
+import { getToken } from "next-auth/jwt";
+import { NextRequest } from "next/server";
+ 
+type API={
+  users: any[],
+
+}
+const Peoples = async(req:NextRequest) => {
+  const token = await getToken({
+    req: req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+ 
+  const res:API = await getRequest(`/user/developers/${token?.id}`);
+   console.log('res is ',res.users);
+  return <MainWrapper classes="flex flex-wrap gap-3">
+       {res.users.map((user,index)=>(
+        <DevCard user={user} key={index}/>
+       ))}
+   
+  </MainWrapper>;
 };
 
-export default page;
+export default Peoples;
