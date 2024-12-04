1c14d86af83eaa0ac26269c5bdd93684fe59c6af
aziz-codes <azizisoffline@gmail.com>
2024-12-04 23:39:49 +0500

    errors fixed

--- client/app/devs/page.tsx
+++ client/app/devs/page.tsx
@@ -1,27 +1,28 @@
-import DevCard from "@/components/uers/dev-card";
+import UserCard from "@/components/users/card";
+ 
 import MainWrapper from "@/layouts/main-wrapper";
 import { getRequest } from "@/services";
 import { getToken } from "next-auth/jwt";
 import { NextRequest } from "next/server";
- 
-type API={
-  users: any[],
 
-}
-const Peoples = async(req:NextRequest) => {
+type API = {
+  users: any[];
+};
+const Peoples = async (req: NextRequest) => {
   const token = await getToken({
     req: req,
     secret: process.env.NEXTAUTH_SECRET,
   });
- 
-  const res:API = await getRequest(`/user/developers/${token?.id}`);
-   console.log('res is ',res.users);
-  return <MainWrapper classes="flex flex-wrap gap-3">
-       {res.users.map((user,index)=>(
-        <DevCard user={user} key={index}/>
-       ))}
-   
-  </MainWrapper>;
+
+  const res: API = await getRequest(`/user/developers/${token?.id}`);
+
+  return (
+    <MainWrapper classes="flex flex-wrap gap-3">
+      {res.users.map((user, index) => (
+         <UserCard   user={user}   key={index}/>
+      ))}
+    </MainWrapper>
+  );
 };
 
 export default Peoples;
