8edbcdff53775703e70c07edf45d63bb29609f99
aziz-codes <azizisoffline@gmail.com>
2024-12-13 00:44:34 +0500

    finalize post modal

--- client/components/custom/post-modal.tsx
+++ client/components/custom/post-modal.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from "react";
 import { DialogHeader } from "@/components/ui/dialog";
 import {
@@ -5,29 +6,53 @@ import {
   AlertDialogCancel,
   AlertDialogContent,
 } from "@/components/ui/alert-dialog";
+import { useRouter } from "next/navigation";
 import { X } from "lucide-react";
 import Image from "next/image";
-import { Post } from "@/types/post";
+import { Post,CommentType } from "@/types/post";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import ReactTimeago from "react-timeago";
 import { customFormatter } from "@/utils/utils";
+import TextBox from "../text-box";
+import { useQuery } from "@tanstack/react-query";
+import { getRequest } from "@/services";
+import Comment from "../comment";
+import CommentDetailed from "../comment-detailed";
 
 interface Props{
   post: Post;
   children?: React.ReactNode
   open: boolean;
   setter: React.Dispatch<React.SetStateAction<boolean>>;
+  setComment: React.Dispatch<React.SetStateAction<string>>;
+  onComment: ()=>void;
+  comment: string;
+  loading: boolean;
 }
 
 const PostModal: React.FC<Props> = ({
   open,
   post,
-  setter
+  setter,
+  setComment,
+  onComment,
+  comment,
+  loading,
+ 
 }) => {
+  const router = useRouter();
+  const {
+    data: comments,
+    error,
+    isLoading,
+  } = useQuery<CommentType[], Error>({
+    queryKey: [`comments/${post._id}`],
+    queryFn: async () => await getRequest(`/post/comment/${post._id}`),
+  });
   return (
     <AlertDialog open={open} onOpenChange={setter}>
     
-      <AlertDialogContent className="sm:max-w-[425px] md:max-w-4xl px-4">
+      <AlertDialogContent className="sm:max-w-[425px] md:max-w-6xl px-4 ">
         <DialogHeader></DialogHeader>
 
         <AlertDialogCancel
@@ -37,8 +62,8 @@ const PostModal: React.FC<Props> = ({
         >
           <X className="h-5 w-5" />
         </AlertDialogCancel>
-        <div className="w-full  flex gap-3 h-[85vh] ">
-          <div className="relative w-full h-full  overflow-hidden rounded-md px-3 border ">
+        <div className="w-full flex gap-3 h-[85vh] ">
+          <div className="relative w-full h-full  overflow-hidden rounded-md px-3">
             <Image
               src={post.image}
               alt="post thumbnail"
@@ -52,11 +77,12 @@ const PostModal: React.FC<Props> = ({
               className="w-full h-auto object-contain rounded-3xl z-10"
             />
           </div>
-          <div className="w-full max-w-xs border flex flex-col">
+          {/* right side  */}
+          <div className="w-full max-w-md  flex flex-col px-4 gap-3">
           <div className="flex items-center gap-1.5">
           <Avatar
             className="cursor-pointer"
-            // onClick={() => router.push(`${post.user.username}`)}
+            onClick={() => router.push(`${post.user.username}`)}
           >
             <AvatarFallback>{post.user.name.slice(0, 2)}</AvatarFallback>
             <AvatarImage src={post.user.avatar} />
@@ -64,20 +90,31 @@ const PostModal: React.FC<Props> = ({
           <div className="flex flex-col -space-y-0.5">
             <p
               className="text-sm font-semibold cursor-pointer text-white"
-              // onClick={() => router.push(`/${post.user.username}`)}
+              onClick={() => router.push(`/${post.user.username}`)}
             >
               {post.user.username}
             </p>
 
             <div className="text-[10px] text-gray-400">
-              <ReactTimeago date={post.createdAt} formatter={customFormatter} />
+              <ReactTimeago date={post.createdAt} formatter={customFormatter} /> ago
             </div>
           </div>
         </div>
-            <div>{post.title}</div>
-            <div>comments</div>
-            <div>like</div>
-            <div>comment box</div>
+            <div className="h-auto max-h-24 pb-4 text-sm">{post.title}</div>
+            <div className=" flex-1 flex flex-col gap-2 max-h-full overflow-y-auto scrollbar-none">
+            {isLoading && "Loading comments"}
+                {comments?.map((comment, index) => (
+                  <CommentDetailed comment={comment} key={index}/>
+                ))}
+            </div>
+            <div className="flex w-full items-center border-b rounded-md px-3 border">
+                <TextBox
+                  comment={comment}
+                  setComment={setComment}
+                  onComment={onComment}
+                  loading={loading}
+                />
+              </div>
           </div>
         </div>
       </AlertDialogContent>
