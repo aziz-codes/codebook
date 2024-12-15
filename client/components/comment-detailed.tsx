38c49b89ae7633588c251938b3c6d409b5e59a44
aziz-codes <azizisoffline@gmail.com>
2024-12-15 23:36:17 +0500

    more options popup issue

--- client/components/comment-detailed.tsx
+++ client/components/comment-detailed.tsx
@@ -18,10 +18,13 @@ interface CommentProps {
   comment: CommentType;
 }
 const CommentDetailed: React.FC<CommentProps> = ({ comment }) => {
-    const router = useRouter();
+  const router = useRouter();
   return (
     <div className="flex space-x-2 py-2   text-white rounded-lg shadow-sm group">
-  <Avatar className="w-8 h-8 cursor-pointer" onClick={()=>router.push(`/${comment.userDetails.username}`)}>
+      <Avatar
+        className="w-8 h-8 cursor-pointer"
+        onClick={() => router.push(`/${comment.userDetails.username}`)}
+      >
         <AvatarImage
           src={comment.userDetails.avatar}
           alt={comment.userDetails.username}
@@ -30,32 +33,41 @@ const CommentDetailed: React.FC<CommentProps> = ({ comment }) => {
       </Avatar>
       <div className="flex-1">
         <div className="flex items-center justify-between">
-        <p className="font-semibold text-slate-300 text-sm cursor-pointer" onClick={()=>router.push(`/${comment.userDetails.username}`)}>
-              {comment.userDetails.username}
-            </p>
+          <p
+            className="font-semibold text-slate-300 text-sm cursor-pointer"
+            onClick={() => router.push(`/${comment.userDetails.username}`)}
+          >
+            {comment.userDetails.username}
+          </p>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <MoreHorizontal className="h-4 w-4 text-gray-500 cursor-pointer hover:text-white hidden group-hover:block" />
+              <MoreHorizontal className="h-4 w-4 text-gray-500 cursor-pointer hover:text-white " />
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
-              <DropdownMenuItem>Edit</DropdownMenuItem>
-              <DropdownMenuItem>Delete</DropdownMenuItem>
-              <DropdownMenuItem>Report</DropdownMenuItem>
+              <DropdownMenuItem className="cursor-pointer hover:bg-bgHover rounded-md">
+                Edit
+              </DropdownMenuItem>
+              <DropdownMenuItem className="cursor-pointer hover:bg-bgHover rounded-md">
+                Delete
+              </DropdownMenuItem>
+              <DropdownMenuItem className="cursor-pointer hover:bg-bgHover rounded-md">
+                Report
+              </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
         <p className="text-xs first-letter:uppercase">{comment.text}</p>
         <div className=" flex items-center space-x-4">
-        <div className="text-xs text-gray-400">
-              <ReactTimeago
-                date={comment.createdAt}
-                formatter={customFormatter}
-              />
-            </div>
+          <div className="text-xs text-gray-400">
+            <ReactTimeago
+              date={comment.createdAt}
+              formatter={customFormatter}
+            />
+          </div>
           <Button variant="link" size="icon" className="!p-0 !w-auto">
             <Heart className="h-4 w-4" />
           </Button>
-     
+
           <Button
             variant="link"
             size="sm"
