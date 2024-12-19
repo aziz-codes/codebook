b18ff76f5f80a2dab5fd4e980d81cfd97c0b593f
aziz-codes <azizisoffline@gmail.com>
2024-12-19 22:46:49 +0500

    added like key to comments schema

--- server/schemas/Comment.js
+++ server/schemas/Comment.js
@@ -4,6 +4,7 @@ const commentSchema = new mongoose.Schema(
       user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
       post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
       text: { type: String, required: true },
+      likes:{type:Number, default:0}
     },
     { timestamps: true }
   );
