f904faa1bd2654b4fca8f828364300292431e2bf
aziz-codes <azizisoffline@gmail.com>
2024-12-20 23:54:17 +0500

    comment react route

--- server/routes/comment.routes.js
+++ server/routes/comment.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteComment, getComments, saveComment } from '../controllers/comment.controller.js';
+import { deleteComment, getComments, handleCommentReact, saveComment } from '../controllers/comment.controller.js';
 
 
 const router = express.Router();
@@ -9,6 +9,7 @@ const router = express.Router();
 router.post("/:postid",saveComment);
 router.get("/:postid",getComments);
 router.delete("/:postid",deleteComment);
+router.post("/react/:commentid",handleCommentReact)
 
 
 export default router;
