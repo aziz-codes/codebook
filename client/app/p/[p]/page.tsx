9724458b176121df2da083bc19e2189931143b40
aziz-codes <azizisoffline@gmail.com>
2025-01-02 23:55:08 +0500

    using global route wrapper

--- client/app/p/[p]/page.tsx
+++ client/app/p/[p]/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import React from "react";
 import { useParams } from "next/navigation";
+import MainWrapper from "@/layouts/main-wrapper";
 
 const SinglePost = () => {
   const { p } = useParams();
 
-  return <div>SinglePost id {p}</div>;
+  return <MainWrapper classes="w-full">hello world</MainWrapper>;
 };
 
 export default SinglePost;
