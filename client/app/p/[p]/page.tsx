4ec1151aeacf409d7073811c24905b9a7aa9f694
aziz-codes <azizisoffline@gmail.com>
2025-01-03 01:02:40 +0500

    singe post route completed for single post only

--- client/app/p/[p]/page.tsx
+++ client/app/p/[p]/page.tsx
@@ -3,10 +3,59 @@ import React from "react";
 import { useParams } from "next/navigation";
 import MainWrapper from "@/layouts/main-wrapper";
 
-const SinglePost = () => {
+import { Post } from "@/types/post";
+import { useQuery } from "@tanstack/react-query";
+import { getRequest } from "@/services";
+import SinglePost from "@/components/Post";
+import { useSession } from "next-auth/react";
+interface Data {
+  post: Post;
+}
+const SinglePage = () => {
   const { p } = useParams();
+  const { data: session } = useSession();
+  const { data, error, isLoading } = useQuery<Data>({
+    queryKey: ["post", p],
+    queryFn: async () => await getRequest(`/post/${p}`),
+  });
 
-  return <MainWrapper classes="w-full">hello world</MainWrapper>;
+  const sessionId = session?.user.id;
+
+  return (
+    <MainWrapper classes="w-full flex justify-center px-4 lg:px-8">
+      <div className="w-full max-w-5xl grid grid-cols-1 lg:grid-cols-3 gap-8 py-8">
+        {/* Left Section: Single Post */}
+        <div className="col-span-2 rounded-lg shadow-md    ">
+          {data && (
+            <SinglePost
+              post={data.post}
+              sessionId={sessionId as string}
+              isSingleRoute
+            />
+          )}
+        </div>
+
+        {/* Right Section: Similar Posts or Other Info */}
+        <div className="border rounded-lg shadow-md p-6  w-full">
+          <h2 className="text-xl font-semibold mb-4">Similar Posts</h2>
+          <ul className="space-y-4">
+            <li className="border-b pb-2">
+              <h3 className="text-gray-800">Similar Post 1</h3>
+              <p className="text-sm text-gray-600">Brief description...</p>
+            </li>
+            <li className="border-b pb-2">
+              <h3 className="text-gray-800">Similar Post 2</h3>
+              <p className="text-sm text-gray-600">Brief description...</p>
+            </li>
+            <li>
+              <h3 className="text-gray-800">Similar Post 3</h3>
+              <p className="text-sm text-gray-600">Brief description...</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </MainWrapper>
+  );
 };
 
-export default SinglePost;
+export default SinglePage;
