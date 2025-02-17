2b605fcdafd8ac4bef52d028114630d2ab14745b
aziz-codes <azizisoffline@gmail.com>
2025-02-18 01:50:04 +0500

    input div changed to shadcn textarea

--- client/components/test/input-div.tsx
+++ client/components/test/input-div.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React, { useRef, useEffect } from 'react';
+import React from "react";
+import { Textarea } from "../ui/textarea";
 
 interface Props {
   content: string;
@@ -7,44 +8,14 @@ interface Props {
 }
 
 const EditableContainer: React.FC<Props> = ({ content, setContent }) => {
-  const divRef = useRef<HTMLDivElement>(null);
-
-  const handleInput = (e: React.FormEvent<HTMLDivElement>) => {
-    // Save the HTML content, which includes <br> for line breaks
-    setContent(e.currentTarget.innerHTML);
-  };
-
-  useEffect(() => {
-    if (divRef.current) {
-      divRef.current.style.height = 'auto'; 
-      divRef.current.style.height = `${divRef.current.scrollHeight}px`;
-    }
-  }, [content]);
-
-  useEffect(()=>{
-    divRef.current?.focus();
-  },[])
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
-    if (e.key === 'Enter') {
-      
-      e.preventDefault();
-      document.execCommand('insertHTML', false, '<br><br>');
-    }
-  };
-
   return (
-    <div className="flex flex-col gap-4 w-full h-auto scrollbar-none transition-all">
-      <div
-        ref={divRef}
-        contentEditable
-        onInput={handleInput}
-        onKeyDown={handleKeyDown}
-        className={`relative p-3 overflow-hidden w-full rounded-lg focus:outline-none resize-none whitespace-pre-wrap
-          ${content === '' ? 'before:content-[attr(data-placeholder)] before:text-gray-400 before:absolute' : ''}`}
-        data-placeholder="What's on your mind today?"
-      ></div>
-    </div>
+    <Textarea
+      placeholder="Share your thoughts."
+      rows={1} // Start small
+      className="w-full resize-none overflow-y-auto max-h-56 !border-none !outline-none !ring-0 scrollbar-none placeholder:text-gray-400 "
+      onChange={(e) => setContent(e.target.value)}
+      value={content}
+    />
   );
 };
 
