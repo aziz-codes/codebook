57d9605e850cfa1996f814b1d0c1e7891d961d53
aziz-codes <azizisoffline@gmail.com>
2025-03-05 22:08:49 +0500

    simple icon spinner

---
+++ client/utils/components/icon-loader.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { LoaderCircle } from "lucide-react";
+
+const IconLoader = () => {
+  return (
+    <LoaderCircle className="h-4 w-4 rounded-full animate-spin duration-300 ease-linear" />
+  );
+};
+
+export default IconLoader;
