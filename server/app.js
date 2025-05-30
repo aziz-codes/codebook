1bffd742fe9f224fa58b9923133fa159733abb9c
aziz-codes <azizisoffline@gmail.com>
2025-05-31 02:20:46 +0500

    base url in cors updated

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "https://codebook-frontend-five.vercel.app",
-  "https://codebook-ss7a.vercel.app/",
+  "https://codebook-ss7a.vercel.app",
   "https://codebook-phi.vercel.app/login",
 ];
 // ✅ Apply Middleware in Correct Order
